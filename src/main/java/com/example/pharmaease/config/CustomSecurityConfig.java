32bf93ef519c385c2102728cd81cdec34301f548
pccipri <popandreiciprian1@gmail.com>
2024-02-04 23:36:35 +0200

    + cors configuration

--- src/main/java/com/example/pharmaease/config/CustomSecurityConfig.java
+++ src/main/java/com/example/pharmaease/config/CustomSecurityConfig.java
@@ -7,6 +7,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class CustomSecurityConfig {
@@ -16,9 +19,24 @@ public class CustomSecurityConfig {
         http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests(requests -> requests
                 .requestMatchers("/api/**")
                 .permitAll())
+                .cors(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 
     @Bean
--- src/main/java/com/example/pharmaease/controller/OrderController.java
+++ src/main/java/com/example/pharmaease/controller/OrderController.java
@@ -88,6 +88,14 @@ public class OrderController {
 
     @DeleteMapping("/{id}")
     void deleteOrder(@PathVariable Integer id) {
+        Order order = orderRepository.findById(id).orElseThrow();
+
+        List<OrderedProduct> orderProducts = order.getOrderedProducts();
+
+        // Delete related orderProducts
+        orderProducts.forEach(orderedProductRepository::delete);
+
+        // Now, delete the order
         orderRepository.deleteById(id);
     }
 
